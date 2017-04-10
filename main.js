6bd1b49274202934c55dc07cc5895677b7ca4a70
v1ym <jpbarker2@gmail.com>
2017-04-10 09:29:33 -0400

    fcac-quiz

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#submit-button").click(function() {
+
+  });
+});
+
+var cat1 = ($("input[@name=q1]:checked").val() != "a");
+var cat2 = ($("input[@name=q2]:checked").val() != "a");
+var cat3 = ($("input[@name=q3]:checked").val() != "a");
+var cat4 = ($("input[@name=q4]:checked").val() != "a");
+var cat5 = ($("input[@name=q5]:checked").val() != "a");
+var cat6 = ($("input[@name=q6]:checked").val() != "a");
+var cat7 = ($("input[@name=q7]:checked").val() != "a");
+var cat8 = ($("input[@name=q8]:checked").val() != "a");
+var cat9 = ($("input[@name=q9]:checked").val() != "a");
+var cat10 = ($("input[@name=q10]:checked").val() != "a");
+var cat11 = ($("input[@name=q11]:checked").val() != "a");
+var cat12 = ($("input[@name=q12]:checked").val() != "a");
+var cat13 = ($("input[@name=q13]:checked").val() != "a");
+var cat14 = ($("input[@name=q14]:checked").val() != "a");
+var cat15 = ($("input[@name=q15]:checked").val() != "a");
